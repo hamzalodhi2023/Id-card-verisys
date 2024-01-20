129f166cd8e839ec62ed4a7fbe22e75bbe356295
Hafsa Khan Lodhi <hafsalodhi2023@gmail.com>
2024-01-20 14:43:35 +0500

    data fetching logic completed

--- main.js
+++ main.js
@@ -1,18 +1,41 @@
+/*********************************************************
+ * ! VARIABLES SECTION STARTED
+ ********************************************************** */
+
 let cardMainDiv = document.querySelector(".card-main-div");
 let searchBtn = document.querySelector("button");
 let closeBtn = document.querySelector(".fa-xmark");
 
-searchBtn.addEventListener("click", function () {
-  cardMainDiv.style.scale = "100%";
-  setTimeout(() => {
-    cardMainDiv.style.scale = "98%";
-    cardMainDiv.style.transition = "0.01s";
-  }, 600);
-  setTimeout(() => {
-    cardMainDiv.style.scale = "100%";
-  }, 1200);
+let idInput = document.querySelector("#idNumber");
+let issuedDataInput = document.querySelector("#issuedData");
+let form = document.querySelector("form");
+
+/*********************************************************
+ * ! VARIABLES SECTION ENDED
+ ********************************************************** */
+
+/**********************************************************
+ * ! DATA FETCHING LOGIC STARTED
+ ********************************************************* */
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  let DataFetchingURL = `
+    https://script.google.com/macros/s/AKfycbxuAw8-mrZUuskDuSKC6Q0yb2L8ezSffIH6HgIOGfdClMJZSwKnyFY5-HldkePsjUy6ZQ/exec`;
+
+  fetch(`${DataFetchingURL}?ID=${encodeURIComponent(idInput.value)}`)
+    .then((res) => {
+      return res.json();
+    })
+    .then((finalRes) => {
+      console.log(finalRes.data[0].Name);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 });
 
-closeBtn.addEventListener("click", function () {
-  cardMainDiv.style.scale = "0%";
-});
+/*********************************************************
+ * ! DATA FETCHING LOGIC ENDED
+ ********************************************************** */
